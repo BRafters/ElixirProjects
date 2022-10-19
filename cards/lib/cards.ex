13bb5dbf86ab45cea3a2b08c76f3f67601755186
Brennan Rafters <brennanrafters@Brennans-Mac-mini.local>
2022-10-19 09:06:20 -0300

    Added some documentation and some pattern matching involved

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Creates a deck of cards, unshuffled
+  """
   def create_deck do
     values = [
       "Ace",
@@ -23,31 +30,63 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Deals a hand of cards from a deck of cards
+
+    `deck` = Deck you are dealing cards from
+    `size_of_hand` = Size of the hand of cards
+  """
   def deal(deck, size_of_hand) do
     Enum.split(deck, size_of_hand)
   end
 
+  @doc """
+    Shuffles a deck of cards
+
+    `deck` = Deck of cards to shuffle
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
-  # Returns a boolean depending if the condition is truthy or falsey
+  @doc """
+    Determines if the deck, contains a card
+  """
   def contains?(card, deck) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Saves a deck of cards to a file
+
+    `deck` = Deck of cards
+    `file_name` = File to write deck to
+  """
   def save(deck, file_name) do
     binary = :erlang.term_to_binary(deck)
     File.write(file_name, binary)
   end
 
-  def load(file_name) do
-    {status, binary} = File.read(file_name)
+  @doc """
+    Loads a deck of cards
 
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "File does not exist"
+    `file_name` = File we are looking to load our deck of cards from
+  """
+  def load(file_name) do
+    case File.read(file_name) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "File does not exist"
     end
-    :erlang.binary_to_term(binary)
+  end
+
+  @doc """
+    Creates a hand of cards
+
+    `hand_size` = Number of cards you'd like in your hand
+  """
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
